608025f120ce72bc316236c2eacd3d4de2f1abe8
djcurry <djcurry@ncsu.edu>
2022-12-12 00:52:09 -0500

    Add Landing and About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent {
+  aboutMe= "I began learning my first programming language in high school. After I realized what I could accomplish, I began learning Go to apply to some of my interests, trading, and cryptocurrency. These projects sparked my love for Computer Science.";
+  name2 = "What can we accomplish together, COMPANY?";
+}
