1fa4262103b88a485ac8794aa7e287d6c1de92c6
Derek Curry (djcurry) <djcurry@ncsu.edu>
2023-03-27 19:36:50 -0400

    add modal

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faClose } from '@fortawesome/free-solid-svg-icons'
+
 
 @Component({
   selector: 'app-landing',
@@ -8,4 +10,5 @@ import { Component } from '@angular/core';
 export class LandingComponent {
   aboutMe= "I began learning my first programming language in high school. After I realized what I could accomplish, I began learning Go to apply to some of my interests, trading, and cryptocurrency. These projects sparked my love for Computer Science.";
   name2 = "What can we accomplish together, COMPANY?";
+  faClose = faClose;
 }
